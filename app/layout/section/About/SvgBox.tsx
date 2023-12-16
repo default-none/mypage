b2a559fecff21d06c67e76623385bc43abe3f922
default-none <foxrain426@gmail.com>
2023-12-16 17:55:36 +0900

    build fix

--- app/layout/section/About/SvgBox.tsx
+++ app/layout/section/About/SvgBox.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
+// import Image from "next/image";
 
-type Props = {
-  SVG: string;
-  alt: string;
-};
+// type Props = {
+//   SVG: string;
+//   alt: string;
+// };
 
-export function SvgBox({ SVG, alt }: Props) {
-  return (
-    <div className="relative w-[75px] h-[75px]">
-      <Image src={SVG} alt={alt} fill />
-    </div>
-  );
-}
+// export function SvgBox({ SVG, alt }: Props) {
+//   return (
+//     <div className="relative w-[75px] h-[75px]">
+//       <Image src={SVG} alt={alt} fill />
+//     </div>
+//   );
+// }
--- app/layout/section/State.tsx
+++ app/layout/section/State.tsx
@@ -1,128 +1,128 @@
-import { history, skillSvg } from "@/app/datas/globals";
-import { SubTitle } from "@/components/text/SubTitle";
-import { useScroller } from "@/hooks";
-import { hookLogic } from "portfolio";
-import { useRef, useState } from "react";
-import { SvgBox } from "@/app/layout/section/about/SvgBox";
-import { MoreButton } from "@/components/button";
+// import { history, skillSvg } from "@/app/datas/globals";
+// import { SubTitle } from "@/components/text/SubTitle";
+// import { useScroller } from "@/hooks";
+// import { hookLogic } from "portfolio";
+// import { useRef, useState } from "react";
+// import { SvgBox } from "@/app/layout/section/about/SvgBox";
+// import { MoreButton } from "@/components/button";
 
-export function State({
-  firstView,
-  setFirstView,
-  secondView,
-  setSecondView,
-}: hookLogic) {
-  const stateRef = useRef<HTMLElement>(null);
-  const [moreLogic, setMoreLogic] = useState(false);
+// export function State({
+//   firstView,
+//   setFirstView,
+//   secondView,
+//   setSecondView,
+// }: hookLogic) {
+//   const stateRef = useRef<HTMLElement>(null);
+//   const [moreLogic, setMoreLogic] = useState(false);
 
-  //공통되는 useScroller의 형식을 component화 하려 했으나 HTMLElment or HTMLDivElemnt type쪽에서 current속성이 존재하지 않는다고 함.
-  useScroller(() => {
-    if (stateRef === null) return;
-    if (stateRef.current === null) return;
-    const stateTop = stateRef.current.offsetTop;
-    const stateHeight = stateRef.current.offsetHeight;
-    if (
-      window.scrollY < stateTop / 2 ||
-      window.scrollY >= stateTop + stateHeight / 2
-    ) {
-      setFirstView(false);
-      setSecondView(false);
-    }
-    if (
-      window.scrollY < stateTop + stateHeight / 2 &&
-      window.scrollY > stateTop - stateHeight / 2
-    ) {
-      setFirstView(true);
-      setSecondView(true);
-    }
-  }, 300);
-  return (
-    <section
-      ref={stateRef}
-      className="life w-[100%] h-[100dvh] flex-col-center"
-    >
-      <div className="history w-[100%]">
-        <SubTitle text={"History"} view={firstView} />
-        <ul className="h-[100%] flex flex-col flex-wrap gap-x-[80px] gap-y-[20px] overflow-hidden mt-[50px]">
-          {history.map((career, id) => {
-            return (
-              <li
-                key={id}
-                className={
-                  (firstView
-                    ? "translate-y-[0%] opacity-100"
-                    : "translate-y-[50%] opacity-0") +
-                  " text-white-base flex justify-between w-[calc(50%-40px)] h-[20%] my-[5px] duration-[.5s]"
-                }
-              >
-                <span>{career.content}</span>
-                <span>{career.day}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="skill w-[100%] mt-[50px] overflow-hidden">
-        <div className="flex items-center mb-[50px]">
-          <SubTitle text={"Skill"} view={secondView} />
-          <MoreButton
-            logic={moreLogic}
-            setLogic={setMoreLogic}
-            view={secondView}
-          />
-        </div>
-        <div
-          className={
-            (secondView ? "opacity-100" : "opacity-0") +
-            " relative h-[220px] duration-[.5s]"
-          }
-        >
-          <ul
-            className={
-              (moreLogic ? " opacity-100 h-[100%]" : " opacity-0 h-[0%]") +
-              " absolute-content m-auto w-[max-content] duration-[.5s] overflow-hidden grid grid-cols-5 gap-y-[20px] top-[50%] left-[50%]"
-            }
-          >
-            {skillSvg.map((svg, id) => {
-              return (
-                <li
-                  key={id}
-                  className=" bg-bg-color2 w-[100px] h-[100px] flex-col-center border-white border-solid border-[2px] mx-[50px]"
-                >
-                  <SvgBox SVG={svg.name} alt={svg.id} />
-                </li>
-              );
-            })}
-          </ul>
-          <ul
-            className={
-              (moreLogic ? "h-[0%] opacity-0" : "h-[100%] opacity-100") +
-              " flex w-[max-content] duration-[.5s] slide overflow-hidden items-center"
-            }
-          >
-            {skillSvg.map((svg, id) => {
-              return (
-                <li
-                  key={id}
-                  className="bg-bg-color2 w-[100px] h-[100px] flex-col-center border-white border-solid border-[2px] mx-[50px]"
-                >
-                  <SvgBox SVG={svg.name} alt={svg.id} />
-                </li>
-              );
-            })}
-            {skillSvg.map((svg) => {
-              return (
-                <li
-                  key={svg.id}
-                  className=" bg-bg-color2 w-[100px] h-[100px] flex-col-center border-white border-solid border-[2px] mx-[50px]"
-                >
-                  <SvgBox SVG={svg.name} alt={svg.id} />
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </div>
-    </section>
-  );
-}
+//   //공통되는 useScroller의 형식을 component화 하려 했으나 HTMLElment or HTMLDivElemnt type쪽에서 current속성이 존재하지 않는다고 함.
+//   useScroller(() => {
+//     if (stateRef === null) return;
+//     if (stateRef.current === null) return;
+//     const stateTop = stateRef.current.offsetTop;
+//     const stateHeight = stateRef.current.offsetHeight;
+//     if (
+//       window.scrollY < stateTop / 2 ||
+//       window.scrollY >= stateTop + stateHeight / 2
+//     ) {
+//       setFirstView(false);
+//       setSecondView(false);
+//     }
+//     if (
+//       window.scrollY < stateTop + stateHeight / 2 &&
+//       window.scrollY > stateTop - stateHeight / 2
+//     ) {
+//       setFirstView(true);
+//       setSecondView(true);
+//     }
+//   }, 300);
+//   return (
+//     <section
+//       ref={stateRef}
+//       className="life w-[100%] h-[100dvh] flex-col-center"
+//     >
+//       <div className="history w-[100%]">
+//         <SubTitle text={"History"} view={firstView} />
+//         <ul className="h-[100%] flex flex-col flex-wrap gap-x-[80px] gap-y-[20px] overflow-hidden mt-[50px]">
+//           {history.map((career, id) => {
+//             return (
+//               <li
+//                 key={id}
+//                 className={
+//                   (firstView
+//                     ? "translate-y-[0%] opacity-100"
+//                     : "translate-y-[50%] opacity-0") +
+//                   " text-white-base flex justify-between w-[calc(50%-40px)] h-[20%] my-[5px] duration-[.5s]"
+//                 }
+//               >
+//                 <span>{career.content}</span>
+//                 <span>{career.day}</span>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </div>
+//       <div className="skill w-[100%] mt-[50px] overflow-hidden">
+//         <div className="flex items-center mb-[50px]">
+//           <SubTitle text={"Skill"} view={secondView} />
+//           <MoreButton
+//             logic={moreLogic}
+//             setLogic={setMoreLogic}
+//             view={secondView}
+//           />
+//         </div>
+//         <div
+//           className={
+//             (secondView ? "opacity-100" : "opacity-0") +
+//             " relative h-[220px] duration-[.5s]"
+//           }
+//         >
+//           <ul
+//             className={
+//               (moreLogic ? " opacity-100 h-[100%]" : " opacity-0 h-[0%]") +
+//               " absolute-content m-auto w-[max-content] duration-[.5s] overflow-hidden grid grid-cols-5 gap-y-[20px] top-[50%] left-[50%]"
+//             }
+//           >
+//             {skillSvg.map((svg, id) => {
+//               return (
+//                 <li
+//                   key={id}
+//                   className=" bg-bg-color2 w-[100px] h-[100px] flex-col-center border-white border-solid border-[2px] mx-[50px]"
+//                 >
+//                   <SvgBox SVG={svg.name} alt={svg.id} />
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//           <ul
+//             className={
+//               (moreLogic ? "h-[0%] opacity-0" : "h-[100%] opacity-100") +
+//               " flex w-[max-content] duration-[.5s] slide overflow-hidden items-center"
+//             }
+//           >
+//             {skillSvg.map((svg, id) => {
+//               return (
+//                 <li
+//                   key={id}
+//                   className="bg-bg-color2 w-[100px] h-[100px] flex-col-center border-white border-solid border-[2px] mx-[50px]"
+//                 >
+//                   <SvgBox SVG={svg.name} alt={svg.id} />
+//                 </li>
+//               );
+//             })}
+//             {skillSvg.map((svg) => {
+//               return (
+//                 <li
+//                   key={svg.id}
+//                   className=" bg-bg-color2 w-[100px] h-[100px] flex-col-center border-white border-solid border-[2px] mx-[50px]"
+//                 >
+//                   <SvgBox SVG={svg.name} alt={svg.id} />
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
